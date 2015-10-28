5f1bc44d14254e23dab97dd5fc8b4af6cab8ebec
Catherine Farman <Catherine.Farman@cfpb.gov>
2015-10-28 01:50:42 -0400

    Revert "Go home"
    This reverts commit b5af4c873bafc95fd30e2a61b57c28726c4b00ec.

--- test/futureValue.js
+++ test/futureValue.js
@@ -6,6 +6,9 @@ var test = require('tape'),
 test('calculate futureValue', function(t) {
   t.plan(3);
 
+
+// $1000 invested for 5 years with simple annual interest of 10% would have a future value of $1,500.00.
+
   var calc1 = futureValue({
     compounding: false,
     rate: 0.10,
@@ -13,7 +16,7 @@ test('calculate futureValue', function(t) {
     years: 5
   });
 
-  t.equal(calc1, 1500, 'correctly calculates future value with simple interest');
+  t.equal(calc1, 1500, 'correctly calculates future Value with simple interest');
 
   var calc2 = futureValue({
     compounding: false,
@@ -22,7 +25,7 @@ test('calculate futureValue', function(t) {
     years: 6
   });
 
-  t.equal(calc2, 1600, 'correctly calculates future value with simple interest');
+  t.equal(calc2, 1600, 'correctly calculates future Value with simple interest');
 
   var calc3 = futureValue({
     compounding: true,
