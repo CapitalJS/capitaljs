720abc4da714a4a102f8804480ba2a7856bc3f29
Catherine Farman <Catherine.Farman@cfpb.gov>
2015-10-27 21:27:05 -0400

    it passed

--- test/futureValue.js
+++ test/futureValue.js
@@ -17,4 +17,14 @@ test('calculate futureValue', function(t) {
   });
 
   t.equal(calc1, 1500, 'correctly calculates future Value with simple interest');
+
+  var calc2 = futureValue({
+    compound: false,
+    rate: 10,
+    startValue: 1000,
+    years: 6
+  });
+
+  t.equal(calc2, 1600, 'correctly calculates future Value with simple interest');
+
 });
