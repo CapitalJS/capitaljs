31e0b410071c3bef88f70da4a08f6ad0e99e8c1e
Catherine Farman <Catherine.Farman@cfpb.gov>
2015-10-28 01:46:30 -0400

    HHI test

---
+++ test/herfindahlIndex.js
@@ -0,0 +1,27 @@
+require('babel/register');
+
+var test = require('tape'),
+    futureValue = require('../src/futureValue');
+
+test('calculate futureValue', function(t) {
+  t.plan(3);
+
+
+// The Herfindahl Index is a measure of the size of firms in relation to the industry and an indicator of the amount of competition among them
+
+  var calc1 = futureValue({
+    compounding: false,
+    rate: 0.10,
+    principal: 1000,
+    years: 5
+  });
+
+  t.equal(calc1, 1500, 'correctly calculates future Value with simple interest');
+
+// HHI = s1^2 + s2^2 + s3^2 + ... + sn^2 (where sn is the market share of the ith firm).
+
+
+
+// Read more: Herfindahl-Hirschman Index (HHI) Definition | Investopedia http://www.investopedia.com/terms/h/hhi.asp#ixzz3pq3k9iHA
+
+});
