dfb65d234333cf42f2e9eb5b674bbd8383d39100
Catherine Farman <Catherine.Farman@cfpb.gov>
2015-11-06 10:25:45 -0500

    descriptive hhi assertions

--- test/herfindahlIndex.js
+++ test/herfindahlIndex.js
@@ -8,15 +8,15 @@ test('calculate Herfindahl Index', function(t) {
 
   var calc1 = herfindahlIndex([30, 60]);
 
-  t.equal(calc1, 4500, 'correctly calculates Herfindahl-Hirschman Index');
+  t.equal(calc1, 4500, 'correctly calculates Herfindahl-Hirschman Index of 2 firms with 30% and 60% market share');
 
   var calc2 = herfindahlIndex([30, 30, 20, 20]);
 
-  t.equal(calc2, 2600, 'correctly calculates Herfindahl-Hirschman Index');
+  t.equal(calc2, 2600, 'correctly calculates Herfindahl-Hirschman Index of 4 firms with 30%, 30%, 20%, 20% market share');
 
   var calc3 = herfindahlIndex([100]);
 
-  t.equal(calc3, 10000, 'correctly calculates Herfindahl-Hirschman Index');
+  t.equal(calc3, 10000, 'correctly calculates Herfindahl-Hirschman Index of a monopoly');
 
   t.throws(function() {
       herfindahlIndex('not a number');
