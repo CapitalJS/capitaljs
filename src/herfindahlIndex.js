681f86af7226a57da9ae37af457187d66ab5bbb3
Catherine Farman <Catherine.Farman@cfpb.gov>
2015-11-05 20:30:46 -0500

    hhi function

---
+++ src/herfindahlIndex.js
@@ -0,0 +1,25 @@
+var enforceNumber = require('./utils/enforce/number');
+
+/**
+ * Calculates Herfindahl-Hirschman Index, a measure of market concentration and competition, used by U.S. governments for antitrust enforcement.
+ * @param {array} marketShare (expressed as percentage value for each firm)
+ * @returns {number} Herfindahl-Hirschman Index (HHI), a number between 0 and 10,000
+ */
+
+function herfindahlIndex(marketShare) {
+  var result = 0;
+
+  enforceNumber(marketShare);
+  if (!marketShare) {
+    throw new Error('Market share array is required and must consist of numbers.');
+  }
+
+  for (var i = 0; i < marketShare.length; i++) {
+    console.log(marketShare[i])
+    result += marketShare[i]*marketShare[i];
+  }
+
+  return result;
+}
+
+module.exports = herfindahlIndex;
